f897d67daba8cfe2232e8f6c71b5cf9e25c6e251
Nikita Slonevskiy <slonevskiynikita@gmail.com>
2026-04-27 23:58:49 +0300

    rational AC

--- classes/rational.cpp
+++ classes/rational.cpp
@@ -11,30 +11,54 @@ class Rational {
 
     public:
         Rational();
+        Rational(int num);
         Rational(int numerator, int denominator);
 
         int Numerator() const;
         int Denominator() const;
 
-        Rational &operator+=(Rational &x);
-        Rational &operator-=(Rational &x);
-        Rational &operator*=(Rational &x);
-        Rational &operator/=(Rational &x);
+        Rational &operator+=(const Rational &x);
+        Rational &operator-=(const Rational &x);
+        Rational &operator*=(const Rational &x);
+        Rational &operator/=(const Rational &x);
 
-        Rational operator+(Rational &x);
-        Rational operator-(Rational &x);
-        Rational operator*(Rational &x);
-        Rational operator/(Rational &x);
+        friend Rational operator+(const Rational &a, const Rational &b);
+        friend Rational operator-(const Rational &a, const Rational &b);
+        friend Rational operator*(const Rational &a, const Rational &b);
+        friend Rational operator/(const Rational &a, const Rational &b);
+
+        Rational operator-() const;
+        Rational operator+() const;
+
+        friend bool operator==(const Rational &a, const Rational &b);
+        friend bool operator!=(const Rational &a, const Rational &b);
 };
 
 void Rational::normalize() {
     int g = std::gcd(numerator, denominator);
     numerator /= g;
     denominator /= g;
+    if(denominator < 0) {
+        denominator *= (-1);
+        numerator *= (-1);
+    }
+}
+
+Rational Rational::operator-() const {
+    return Rational(-numerator, denominator);
+}
+
+Rational Rational::operator+() const {
+    return Rational(numerator, denominator);
 }
 
 Rational::Rational() {
     numerator = 0;
+    denominator = 1;
+}
+
+Rational::Rational(int num) {
+    numerator = num;
     denominator = 1;
 }
 
@@ -47,42 +71,50 @@ Rational::Rational(int numerator, int denominator) {
 int Rational::Numerator() const { return numerator; }
 int Rational::Denominator() const { return denominator; }
 
-Rational& Rational::operator+=(Rational &x) {
+Rational& Rational::operator+=(const Rational &x) {
     numerator = numerator * x.denominator + x.numerator * denominator;
     denominator *= x.denominator;
     normalize();
     return *this;
 }
-Rational& Rational::operator*=(Rational &x) {
+Rational& Rational::operator*=(const Rational &x) {
     numerator *= x.numerator;
     denominator *= x.denominator;
     normalize();
     return *this;
 }
-Rational& Rational::operator-=(Rational &x) {
+Rational& Rational::operator-=(const Rational &x) {
     numerator = numerator * x.denominator - x.numerator * denominator;
     denominator *= x.denominator;
     normalize();
     return *this;
 }
-Rational& Rational::operator/=(Rational &x) {
+Rational& Rational::operator/=(const Rational &x) {
     numerator *= x.denominator;
     denominator *= x.numerator;
     normalize();
     return *this;
 }
 
-Rational Rational::operator+(Rational &x) {
-    return Rational(numerator * x.denominator + x.numerator * denominator, denominator * x.denominator);
+Rational operator+(const Rational &a, const Rational &b) {
+    return Rational(a.numerator * b.denominator + b.numerator * a.denominator, a.denominator * b.denominator);
 }
-Rational Rational::operator*(Rational &x) {
-    return Rational(numerator * x.numerator, denominator * x.denominator);
+Rational operator*(const Rational &a, const Rational &b) {
+    return Rational(a.numerator * b.numerator, a.denominator * b.denominator);
 }
-Rational Rational::operator-(Rational &x) {
-    return Rational(numerator * x.denominator - x.numerator * denominator, denominator * x.denominator);
+Rational operator-(const Rational &a, const Rational &b) {
+    return Rational(a.numerator * b.denominator - b.numerator * a.denominator, a.denominator * b.denominator);
 }
-Rational Rational::operator/(Rational &x) {
-    return Rational(numerator * x.numerator, denominator * x.numerator);
+Rational operator/(const Rational &a, const Rational &b) {
+    return Rational(a.numerator * b.denominator, a.denominator * b.numerator);
+}
+
+bool operator==(const Rational &a, const Rational &b) {
+    return (a.denominator == b.denominator and a.numerator == b.numerator);
+}
+
+bool operator!=(const Rational &a, const Rational &b) {
+    return (a.denominator != b.denominator or a.numerator != b.numerator);
 }
 
 std::ostream &operator<<(std::ostream &fout, Rational &x) {
@@ -91,10 +123,9 @@ std::ostream &operator<<(std::ostream &fout, Rational &x) {
 }
 
 int main() { 
-    Rational a(1, 2), b(1, 2);
-    Rational c = a * b;
-    a += b;
-    std::cout << c;
+    Rational a, b(5, 2);
+    Rational c = (a + 2) * b;
+    std::cout << c << "\n";
 
     return 0; 
 }
